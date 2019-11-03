5d1460964423418e7a416a7fb348e55ab8273a65
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-03 13:44:20 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,3 +1,6 @@
-first_number = 0
-seccond_number = 0 
-
+first_number = 
+second_number = 
+sum = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number/second_number
