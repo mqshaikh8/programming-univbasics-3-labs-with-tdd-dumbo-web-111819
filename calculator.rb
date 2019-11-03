89a644beb2dea7b92fac4d6b3db170c3e45b4441
Mohammed shaikh <mqshaikh8@gmail.com>
2019-11-03 13:45:04 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,5 @@
-first_number = 
-second_number = 
+first_number = 0
+second_number = 1 
 sum = first_number + second_number
 difference = first_number - second_number
 product = first_number * second_number
